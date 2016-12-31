2c8f5d2c49426ba03ad76a9bf07c53b8aeee7a11
Phil Kunz <gitlab@philkunz.com>
2016-12-31 19:54:32 +0100

    cleanup (+1 squashed commit) Squashed commits: [192abb9] cleanup

--- ts/index.ts
+++ ts/index.ts
@@ -1,11 +1,11 @@
-import "typings-global";
+import 'typings-global'
 
-import * as plugins from "./beautylog.plugins";
+import * as plugins from './beautylog.plugins'
 
 // export classes
-export {Ora} from "./beautylog.classes.ora";
+export {Ora} from './beautylog.classes.ora'
 
 // export methods
-export {dir,error,info,log,logReduced,ok,success,warn,newLine} from "./beautylog.log";
-export {figlet,figletSync} from "./beautylog.figlet";
-export {remote} from "./beautylog.remote";
+export {dir,error,info,log,logReduced,ok,success,warn,newLine} from './beautylog.log'
+export {figlet,figletSync} from './beautylog.figlet'
+export {remote} from './beautylog.remote'

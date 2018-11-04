5dd07e34edc886bbc92492f49988620661b412a9
Phil Kunz <github@philkunz.com>
2018-11-04 13:26:56 +0100

    fix(core): update

--- ts/sl.destlocal.plugins.ts
+++ ts/sl.destlocal.plugins.ts
@@ -1,9 +1,7 @@
 import * as consolecolor from '@pushrocks/consolecolor';
-import * as lodash from 'lodash';
 import * as smartenv from 'smartenv';
 import * as smartlogInterfaces from '@pushrocks/smartlog-interfaces';
 import * as smartpromise from '@pushrocks/smartpromise';
-let figlet = require('figlet');
 let ora = require('ora');
 
-export { consolecolor, lodash, smartenv, smartlogInterfaces, smartpromise, figlet, ora };
+export { consolecolor, smartenv, smartlogInterfaces, smartpromise, ora };

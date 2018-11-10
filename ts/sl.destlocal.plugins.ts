0712d47155d62b180c210e4c716234b702202bea
Phil Kunz <github@philkunz.com>
2018-11-10 23:29:35 +0100

    fix(dependencies): remove smartenv

--- ts/sl.destlocal.plugins.ts
+++ ts/sl.destlocal.plugins.ts
@@ -1,7 +1,6 @@
 import * as consolecolor from '@pushrocks/consolecolor';
-import * as smartenv from 'smartenv';
 import * as smartlogInterfaces from '@pushrocks/smartlog-interfaces';
 import * as smartpromise from '@pushrocks/smartpromise';
 let ora = require('ora');
 
-export { consolecolor, smartenv, smartlogInterfaces, smartpromise, ora };
+export { consolecolor, smartlogInterfaces, smartpromise, ora };
